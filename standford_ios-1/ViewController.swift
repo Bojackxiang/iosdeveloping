1356cfe04eb559eae2df9edcecaf4c11597bbab5
Weijie Xiang <weijiexiang@Weijies-MacBook-Pro.local>
2018-07-27 14:30:21 +1000

    Initial Commit

---
+++ standford_ios-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  standford_ios-1
+//
+//  Created by Weijie Xiang on 27/7/18.
+//  Copyright © 2018 Weijie Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
